355521c184a57868c6ce30743ca5d63839f0e46e
Fodjik <granfodje@gmail.com>
2025-07-16 03:41:01 +0300

    Update server.js

--- server.js
+++ server.js
@@ -40,11 +40,13 @@ app.listen(process.env.PORT || 3000, () => {
   console.log('Server running...');
 });
 
+const path = require('path');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
 
 
 app.post('/objects', async (req, res) => {
